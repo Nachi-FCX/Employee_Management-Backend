8fdee3a08fcacb82842b3a15ab7cfc03f71763b6
Tharani-ux <127069926+Tharani-ux@users.noreply.github.com>
2026-01-13 14:34:01 +0530

    login module is developed (auth) with JWT

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
-import app from "./app";
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
