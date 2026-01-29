622c0db12cc0372bbe6d799deb21108178cba64e
Tharani-ux <127069926+Tharani-ux@users.noreply.github.com>
2026-01-29 18:29:26 +0530

    repet

--- src/routes/rootAuth.routes.ts
+++ src/routes/rootAuth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { rootSignup } from "../controllers/rootSignup.controller";
 import { createCompany } from "../controllers/companySetup.controller";
-import { rootAuth } from "../middleware/auth.middleware"; 
+import { unifiedAuth } from "../middleware/auth.middleware"; 
 
 const router = Router();
 
 router.post("/signup", rootSignup);  
-router.post("/create-company", rootAuth, createCompany);
+router.post("/create-company", unifiedAuth, createCompany);
 
 export default router;
