90b36ae71c7b50c0a6276103d9301b9898095411
Tharani-ux <127069926+Tharani-ux@users.noreply.github.com>
2026-02-04 15:05:53 +0530

    in companycontroller add  update,delete,list added

--- src/routes/rootAuth.routes.ts
+++ src/routes/rootAuth.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { rootSignup } from "../controllers/rootSignup.controller";
-import { createCompany } from "../controllers/companySetup.controller";
+import { createCompany, deleteCompany, getCompanies, updateCompany } from "../controllers/company.controller";
 import { RootValidation } from "../middleware/root.middleware";
 
 const router = Router();
 
 router.post("/signup", rootSignup);  
 router.post("/create-company", RootValidation, createCompany);
+router.get("/company", RootValidation, getCompanies);
+router.put("/company/:id", RootValidation, updateCompany);
+router.delete("/company/:id", RootValidation, deleteCompany);
 
 export default router;
